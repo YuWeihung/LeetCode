8be7eeb7962c9d63753f1aec40942c4168e703bd
Yu Weihong <yuweihongchn@outlook.com>
2022-10-03 16:43:39 +0800

    0083

---
+++ 83.删除排序链表中的重复元素.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=cpp
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        if (head == nullptr) {
+            return head;
+        }
+        ListNode *cur = head;
+        // 遍历链表，注意本题没有释放被删除元素的内存
+        while (cur->next != nullptr) {
+            if (cur->val == cur->next->val) {
+                cur->next = cur->next->next;
+            } else {
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
+// @lc code=end

7a99213696d7371a631b8496e359cd2e181e9dae
Yu Weihong <yuweihongchn@outlook.com>
2022-10-24 09:43:01 +0800

    0094

--- 94.二叉树的中序遍历.cpp
+++ 94.二叉树的中序遍历.cpp
@@ -4,18 +4,10 @@
  * [94] 二叉树的中序遍历
  */
 
+#include "tree_node.h"
 #include <stack>
 #include <vector>
 using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
 
 // @lc code=start
 /**

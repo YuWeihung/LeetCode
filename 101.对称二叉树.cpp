b26ac8185928067df3db637a55dfe74d2dc5b83c
Yu Weihong <yuweihongchn@outlook.com>
2022-10-24 09:43:12 +0800

    0101

--- 101.对称二叉树.cpp
+++ 101.对称二叉树.cpp
@@ -4,20 +4,9 @@
  * [101] 对称二叉树
  */
 
+#include "tree_node.h"
 #include <queue>
 using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
-};
 
 // @lc code=start
 /**

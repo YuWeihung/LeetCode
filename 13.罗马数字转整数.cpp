9203ee9004f1502664049250abaa9e717b7d2ca6
Yu Weihong <yuweihongchn@outlook.com>
2022-11-07 23:07:01 +0800

    0013

--- 13.罗马数字转整数.cpp
+++ 13.罗马数字转整数.cpp
@@ -11,8 +11,15 @@ using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> values = {{'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
-                                           {'C', 100}, {'D', 500}, {'M', 1000}};
+        unordered_map<char, int> values{
+            {'I',    1},
+            {'V',    5},
+            {'X',   10},
+            {'L',   50},
+            {'C',  100},
+            {'D',  500},
+            {'M', 1000}
+        };
         int ans = 0;
         for (int i = 0; i < s.length(); i++) {
             int value = values[s[i]];
